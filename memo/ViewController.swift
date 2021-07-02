eae1ba32b930454cfbfd6ca23fe4e20e1f7e305f
Tamako Aiba <t.aiba@4nonome.com>
2021-07-02 09:05:58 +0900

    メモアプリ完成。

--- memo/ViewController.swift
+++ memo/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var textField1:UITextField!
+    @IBOutlet var textField2:UITextField!
+    @IBOutlet var textView:UITextView!
 
+    
+    let saveData = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let value = UserDefaults.standard.string(forKey: "WORD")
+        let value1 = UserDefaults.standard.string(forKey: "WORD1")
+        let value2 = UserDefaults.standard.string(forKey: "WORD2")
+        textField1.text = value
+        textField2.text = value1
+        textView.text = value2
     }
-
+    @IBAction func save(){
+      
+        UserDefaults.standard.set(textField1.text, forKey: "WORD")
+        UserDefaults.standard.set(textField2.text, forKey: "WORD1")
+        UserDefaults.standard.set(textView.text, forKey: "WORD2")
+       
+    }
 
 }
 
